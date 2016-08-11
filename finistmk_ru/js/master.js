add899ffdab6db9da947585549cfd8b2491bede5
tov-jukov <tov-jukof@ya.ru>
2016-08-11 16:43:07 +0500

    Нашелся шрифт из логотипа
    Ижица цирилик

--- finistmk_ru/js/master.js
+++ finistmk_ru/js/master.js
@@ -34,7 +34,7 @@ jQuery( document ).ready( function( $ ) {
 	});
 	
 	// Image Lightbox
-	$("a[rel^='prettyPhoto']").prettyPhoto({overlay_gallery: true,social_tools: false,theme:'light_square'});
+	$("a[rel^='prettyPhoto']").prettyPhoto({overlay_gallery: true,social_tools: false});
 	  
 	// Fixed Top bar
 	$(window).bind('scroll', function() {
