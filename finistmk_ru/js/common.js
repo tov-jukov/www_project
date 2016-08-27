d7b7c4a7d93ba99c6e2a0b5e7b12564f4200e1bc
tov-jukov <tov-jukof@ya.ru>
2016-08-27 23:19:48 +0500

    add

---
+++ finistmk_ru/js/common.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+
+	$(".popup").magnificPopup();
+
+	//Цели для Яндекс.Метрики и Google Analytics
+	$(".count_element").on("click", (function() {
+		ga("send", "event", "goal", "goal");
+		yaCounterXXXXXXXX.reachGoal("goal");
+		return true;
+	}));
+
+	//Аякс отправка форм
+	//Документация: http://api.jquery.com/jquery.ajax/
+	$("#form").submit(function() {
+		$.ajax({
+			type: "POST",
+			url: "mail.php",
+			data: $(this).serialize()
+		}).done(function() {
+			alert("Спасибо за заявку!");
+			setTimeout(function() {
+				$.magnificPopup.close();
+			}, 1000);
+		});
+		return false;
+	});
+	
+});
